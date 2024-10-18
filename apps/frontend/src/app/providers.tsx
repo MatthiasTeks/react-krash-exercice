bcb9ee8eb84466f5f20ff5f2b72e532ee924327e
Matthias <matthias.vimbert@gmail.com>
2024-10-18 17:51:28 +0200

    fix(front): sync providers

--- apps/frontend/src/app/providers.tsx
+++ apps/frontend/src/app/providers.tsx
@@ -1,10 +1,19 @@
 'use client';
 
+import { makeStore } from '@/lib/store';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import React, { useState } from 'react';
+import React, { useRef, useState } from 'react';
+import { Provider } from 'react-redux';
 
 export default function Providers({ children }: { children: React.ReactNode }) {
   const [queryClient] = useState(() => new QueryClient());
+  const storeRef = useRef(makeStore());
 
-  return <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>;
+  return (
+    <QueryClientProvider client={queryClient}>
+      <Provider store={storeRef.current}>
+        {children}
+      </Provider>
+    </QueryClientProvider>
+    );
 }
