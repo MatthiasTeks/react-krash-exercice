b4626757a5e9f2722e3998c8e4189817a5f755b2
Matthias <matthias.vimbert@gmail.com>
2024-10-18 17:52:44 +0200

    fix(front): clean default values and test functions

--- apps/frontend/src/lib/store.ts
+++ apps/frontend/src/lib/store.ts
@@ -1,26 +1,8 @@
 import { configureStore } from '@reduxjs/toolkit';
-import { createSlice } from '@reduxjs/toolkit';
-
-const counterSlice = createSlice({
-  name: 'counter',
-  initialState: { value: 0 },
-  reducers: {
-    increment: (state) => {
-      state.value += 1;
-    },
-    decrement: (state) => {
-      state.value -= 1;
-    },
-  },
-});
-
-export const { increment, decrement } = counterSlice.actions;
 
 export const makeStore = () => {
   return configureStore({
-    reducer: {
-      counter: counterSlice.reducer,
-    },
+    reducer: {},
   });
 };
 
