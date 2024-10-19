9032c06fc35a345eed5b24407c4683b99808fa29
Matthias <matthias.vimbert@gmail.com>
2024-10-19 18:18:52 +0200

    fix(back): clean-code

--- apps/backend/src/middlewares/authentication.ts
+++ apps/backend/src/middlewares/authentication.ts
@@ -1,20 +1,20 @@
 import Hapi from '@hapi/hapi';
-import jwt, { JwtPayload } from 'jsonwebtoken';
+import jwt from 'jsonwebtoken';
 
 export const validateAuth = (request: Hapi.Request, h: Hapi.ResponseToolkit) => {
-    const authorization = request.headers.authorization;
+  const authorization = request.headers.authorization;
 
-    if (!authorization) {
-        return h.response({ error: 'Unauthorized' }).code(401).takeover();
-    }
+  if (!authorization) {
+    return h.response({ error: 'Unauthorized' }).code(401).takeover();
+  }
 
-    const token = authorization.split(' ')[1];
+  const token = authorization.split(' ')[1];
 
-    try {
-        jwt.verify(token, process.env.SECRET_KEY as string);
-    } catch (err) {
-        return h.response({ error: 'Invalid token' }).code(401).takeover();
-    }
+  try {
+    jwt.verify(token, process.env.SECRET_KEY as string);
+  } catch (err) {
+    return h.response({ error: 'Invalid token' }).code(401).takeover();
+  }
 
-    return h.continue;
+  return h.continue;
 };
